e2285c217fab532798096cdd2291a3f6954dc439
Bilousov Anton <anton.bilousov@luxoft.com>
2018-05-19 00:50:14 +0300

    Added records page

--- src/components/user-screens/article.js
+++ src/components/user-screens/article.js
@@ -1,9 +1,35 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { addToArticle } from '../../action/add_article';
-import { Route} from 'react-router-dom'
+import { Route} from 'react-router-dom';
+import axios from 'axios';
 
 class Article extends Component {
+    constructor() {
+        super();
+        this.state = {
+            articles: []
+        }
+    }
+    componentDidMount() {
+        axios.get('api/records/', {
+            params: {
+                userId: this.props.user.data.userId
+            }
+        }).then((response) => {
+            this.setState(() => {
+                return {
+                    articles:  response.data
+                }
+            })
+        }).catch((err) => {
+            console.log(err.response.data);
+        })
+    }
+    getTime(milisec) {
+        let time = new Date(milisec);
+        return time.toString();
+    }
     get header() {
         return <thead>
                 <tr>
@@ -20,11 +46,11 @@ class Article extends Component {
                 <table className="table table-dark">
                     {this.header}
                     <tbody>
-                        {this.props.some.map((item, index) => {
+                        {this.state.articles.map((item, index) => {
                             return <tr key={index}>
                                 <th scope="row">{index + 1}</th>
-                                <td>{item}</td>
-                                <td>Otto</td>
+                                <td>{item.title}</td>
+                                <td>{this.getTime(item.startDate)}</td>
                                 <td>@mdo</td>
                             </tr>
                         })}
@@ -34,7 +60,7 @@ class Article extends Component {
         )
     }
 }
-const stateCurrent = (state) => ({some: state.article});
+const stateCurrent = (state) => ({user: state.authorization});
 const dispatchCurrent = (dispatch) => {
   return {
     add: (item) => {
--- src/components/user-screens/creator.js
+++ src/components/user-screens/creator.js
@@ -8,8 +8,18 @@ class Creator extends Component {
     }
     render() {
         return (
-            <div className="col-3">
-                <input ref={(input) => this._input = input} />
+            <div className="col-3">              
+                <div className="input-group input-group-sm mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="inputGroup-sizing-sm">Small</span>
+                    </div>
+                    <input type="text" ref={(input) => this._input = input}
+                            className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"  />
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
                 <button onClick={this.addArticle.bind(this)}> Add </button>
             </div>
         )
